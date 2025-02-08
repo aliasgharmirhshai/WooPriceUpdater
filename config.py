b3831e6ca109ba5d59824b0a6c8d30a6f16bd7ee
aliasgharmirhshai <lmyrshahy3@gmail.com>
2025-02-08 16:25:37 +0330

    start project 🚀

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+WC_API_URL = "https://<your-site>/wp-json/wc/v3/products"
+WC_CONSUMER_KEY = os.getenv("WC_CONSUMER_KEY")
+WC_CONSUMER_SECRET = os.getenv("WC_CONSUMER_SECRET")
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+}
